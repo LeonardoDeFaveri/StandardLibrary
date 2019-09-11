7715af4f755048caf96be5ef9ca4b38563ce2ced
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2019-09-11 21:32:30 +0200

    corretto gli errori nelle macro stringToInt ed intToString

--- test.asm
+++ test.asm
@@ -1,8 +1,7 @@
 %include "./standardlib.inc"
 
 section .data
-    filename db "./file.txt", 0
-    msg      db "Ciao",10, "come va?"
+    msg      db "123456"
 
 section .bss
     buffer resb 100
@@ -11,12 +10,6 @@ section .text
     global _start:
 
     _start:
-        fileOpen filename, O_APPEND+O_RDWR, 0777o
-        push    rax
-        fileRead [rsp], buffer, 100
-        filePrintLine [rsp], msg
-        fileMoveToBeginning [rsp]
-        fileRead [rsp], buffer, 100
-        fileClose [rsp]
-        printLine buffer
+        stringToInt msg
+        printLineInt r11, buffer
         exit
